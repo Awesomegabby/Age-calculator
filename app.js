1bd14480a37a6834c9f393dabbe56acb7791d53e
Shemuel Chukwukadibia <shemmy80@gmail.com>
2024-04-17 01:22:44 +0100

    Add javascript

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const calculateAge = function () {
+  const dobInput = document.getElementById('dob');
+  const calculateBtn = document.getElementById('calculate-btn');
+  const resultDiv = document.getElementById('result');
+
+  calculateBtn.addEventListener('click', function () {
+    const dob = new Date(dobInput.value);
+    const ageInMinutes = Date.now() - dob.getTime();
+    const ageDate = new Date(ageInMinutes);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    resultDiv.textContent = `You are ${age} year${age !== 1 ? 's' : ''} old`;
+  });
+};
+
+calculateAge();
